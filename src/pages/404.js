6a6954cefaa1fab9603b610f2599f4405b5d8708
Goalof <68434460+Goalof@users.noreply.github.com>
2021-03-17 17:42:45 +0600

    Changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -43,7 +43,7 @@ export default (() => {
 		</Link>
 		<RawHtml>
 			<style place={"endOfHead"} rawKey={"603e751a50a2640bdf3f75d3"}>
-				{"@import url(\"/fonts/stylesheet.css\")\n@media (min-width: 1200px) {\n* {\n    scroll-behavior: smooth;\n    }\n    }\n:focus {\n    outline: none;\n}\n@font-face {\n  font-display: auto !important;\n}"}
+				{"@import url(\"/stylesheet.css\")\n@media (min-width: 1200px) {\n* {\n    scroll-behavior: smooth;\n    }\n    }\n:focus {\n    outline: none;\n}\n@font-face {\n  font-display: auto !important;\n}"}
 			</style>
 		</RawHtml>
 	</Theme>;
